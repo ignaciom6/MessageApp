177bae1963dc7fc23fe8f08d685aa12ce79766b8
Ignacio Mariani <ignaciom6@hotmail.com>
2016-05-05 09:02:14 -0300

    32. NewGroupViewController User Interface

--- WhaleTalk/NewGroupViewController.swift
+++ WhaleTalk/NewGroupViewController.swift
@@ -11,14 +11,98 @@ import CoreData
 
 class NewGroupViewController: UIViewController
 {
+    let kMaxLength = 25
+    
     var context: NSManagedObjectContext?
     var chatCreationDelegate: ChatCreationDelegate?
+    
+    private let subjectField = UITextField()
+    private let characterNumberLabel = UILabel()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         title = "New Group"
+        view.backgroundColor = UIColor.whiteColor()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: #selector(NewGroupViewController.cancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: #selector(NewGroupViewController.next))
+        updateNextButton(forCharCount: 0)
+        
+        subjectField.placeholder = "Group Subject"
+        subjectField.delegate = self
+        subjectField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(subjectField)
+        updateCharacterLabel(forCharCount: 0)
+        
+        characterNumberLabel.textColor = UIColor.lightGrayColor()
+        characterNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        subjectField.addSubview(characterNumberLabel)
+        
+        let bottomBorder = UIView()
+        bottomBorder.backgroundColor = UIColor.lightGrayColor()
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        subjectField.addSubview(bottomBorder)
+        
+        let constraints:[NSLayoutConstraint] = [
+            subjectField.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 20),
+            subjectField.leadingAnchor.constraintEqualToAnchor(view.layoutMarginsGuide.leadingAnchor),
+            subjectField.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            bottomBorder.widthAnchor.constraintEqualToAnchor(subjectField.widthAnchor),
+            bottomBorder.bottomAnchor.constraintEqualToAnchor(subjectField.bottomAnchor),
+            bottomBorder.leadingAnchor.constraintEqualToAnchor(subjectField.leadingAnchor),
+            bottomBorder.heightAnchor.constraintEqualToConstant(1),
+            characterNumberLabel.centerYAnchor.constraintEqualToAnchor(subjectField.centerYAnchor),
+            characterNumberLabel.trailingAnchor.constraintEqualToAnchor(subjectField.layoutMarginsGuide.trailingAnchor),
+            ]
+        NSLayoutConstraint.activateConstraints(constraints)
+    }
+    
+    func cancel()
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func next()
+    {
+        
+    }
+    
+    func updateCharacterLabel(forCharCount length: Int)
+    {
+        characterNumberLabel.text = String(kMaxLength - length)
+    }
+    
+    func updateNextButton(forCharCount length:Int)
+    {
+        if length == 0
+        {
+            navigationItem.rightBarButtonItem?.tintColor = UIColor.lightGrayColor()
+            navigationItem.rightBarButtonItem?.enabled = false
+        }
+        else
+        {
+            navigationItem.rightBarButtonItem?.tintColor = view.tintColor
+            navigationItem.rightBarButtonItem?.enabled = true
+        }
     }
 
+}
+
+extension NewGroupViewController: UITextFieldDelegate
+{
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool
+    {
+        let currentCharacterCount = textField.text?.characters.count ?? 0
+        let newLength = currentCharacterCount + string.characters.count - range.length
+        
+        if newLength <= kMaxLength
+        {
+            updateCharacterLabel(forCharCount:newLength)
+            updateNextButton(forCharCount:newLength)
+            return true
+        }
+        
+        return false
+    }
 }
