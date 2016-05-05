7997e48db1d79e255d7812eeb39528a21646e90c
Ignacio Mariani <ignaciom6@hotmail.com>
2016-05-05 19:28:47 -0300

    35. Setup New Participants User Interface

--- WhaleTalk/NewGroupParticipantsViewController.swift
+++ WhaleTalk/NewGroupParticipantsViewController.swift
@@ -14,17 +14,78 @@ class NewGroupParticipantsViewController: UIViewController
     var context: NSManagedObjectContext?
     var chat: Chat?
     var chatCreationDelegate: ChatCreationDelegate?
+    
+    private var searchField: UITextField!
+    
+    private let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    private let cellIdentifier = "ContactCell"
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         title = "Add Participants"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .Plain, target: self, action: "createChat")
+        showCreateButton(false)
+        
+        automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        
+        searchField = createSearchField()
+        tableView.tableHeaderView = searchField
+        
+        fillWithView(tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func createSearchField() -> UITextField
+    {
+        let searchField = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 50))
+        searchField.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        searchField.placeholder = "Type contact name"
+        
+        let holderView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        searchField.leftView = holderView
+        searchField.leftViewMode = .Always
+        
+        let image = UIImage(named: "contact_icon")?.imageWithRenderingMode(.AlwaysTemplate)
+        
+        let contactImage = UIImageView(image: image)
+        contactImage.tintColor = UIColor.darkGrayColor()
+        
+        holderView.addSubview(contactImage)
+        contactImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints:[NSLayoutConstraint] = [
+            contactImage.widthAnchor.constraintEqualToAnchor(holderView.widthAnchor, constant:-20),
+            contactImage.heightAnchor.constraintEqualToAnchor(holderView.heightAnchor, constant:-20),
+            contactImage.centerXAnchor.constraintEqualToAnchor(holderView.centerXAnchor),
+            contactImage.centerYAnchor.constraintEqualToAnchor(holderView.centerYAnchor)
+        ]
+        NSLayoutConstraint.activateConstraints(constraints)
+        
+        return searchField
+    }
+    
+    private func showCreateButton(show: Bool)
+    {
+        if show
+        {
+            navigationItem.rightBarButtonItem?.tintColor = view.tintColor
+            navigationItem.rightBarButtonItem?.enabled = true
+        }
+        else
+        {
+            navigationItem.rightBarButtonItem?.tintColor = UIColor.lightGrayColor()
+            navigationItem.rightBarButtonItem?.enabled = false
+        }
     }
 
 }
