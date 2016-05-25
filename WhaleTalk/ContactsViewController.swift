88249b7ee214417b3318e567a1634ada52da05bb
Ignacio Mariani <ignaciom6@hotmail.com>
2016-05-25 17:19:58 -0300

    56. Add New Contact

--- WhaleTalk/ContactsViewController.swift
+++ WhaleTalk/ContactsViewController.swift
@@ -74,7 +74,9 @@ class ContactsViewController: UIViewController, ContextViewController, TableView
     
     func newContact()
     {
-        
+        let vc = CNContactViewController(forNewContact: nil)
+        vc.delegate = self
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     func configureCell(cell:UITableViewCell, atIndexPath indexPath:NSIndexPath)
@@ -149,5 +151,17 @@ extension ContactsViewController: UITableViewDelegate
         guard let contact = fetchedResultsController?.objectAtIndexPath(indexPath) as? Contact else {return}
         selectedContact(contact)
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+}
+
+extension ContactsViewController: CNContactViewControllerDelegate
+{
+    func contactViewController(viewController: CNContactViewController, didCompleteWithContact cnContact: CNContact?)
+    {
+        if cnContact == nil
+        {
+            navigationController?.popViewControllerAnimated(true)
+            return
+        }
     }
 }
