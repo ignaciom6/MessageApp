3b2d82185590e69df78851688c525a7561d89dce
Ignacio Mariani <ignaciom6@hotmail.com>
2016-07-11 17:39:04 -0300

    65. Open Chat From FavoritesViewController

--- Chat.swift
+++ Chat.swift
@@ -41,4 +41,25 @@ class Chat: NSManagedObject {
     {
         mutableSetValueForKey("participants").addObject(contact)
     }
+    
+    static func existing(directWith contact:Contact, inContext context:NSManagedObjectContext)->Chat?
+    {
+        let request = NSFetchRequest(entityName: "Chat")
+        request.predicate = NSPredicate(format: "ANY participants = %@ AND participants.@count = 1", contact)
+        do
+        {
+            guard let results = try context.executeFetchRequest(request) as? [Chat] else {return nil}
+            return results.first
+        }
+        catch{}
+        
+        return nil
+    }
+    
+    static func new(directWith contact:Contact, inContext context:NSManagedObjectContext)->Chat
+    {
+        let chat = NSEntityDescription.insertNewObjectForEntityForName("Chat", inManagedObjectContext: context) as! Chat
+        chat.add(participant: contact)
+        return chat
+    }
 }
